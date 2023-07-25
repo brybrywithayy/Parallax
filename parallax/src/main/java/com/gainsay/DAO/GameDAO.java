6eba87f4de0be7f57aedbd4cffcdb2fa730340b9
brybrywithayy <brybrywithayy@gmail.com>
2023-07-25 18:44:42 -0400

    Add comments for DAO class

--- parallax/src/main/java/com/gainsay/DAO/GameDAO.java
+++ parallax/src/main/java/com/gainsay/DAO/GameDAO.java
@@ -5,4 +5,28 @@ public class GameDAO {
     /*
      * Contains all methods to access the database
      */
+
+    // get faction
+
+    // get pilot
+
+    // get all pilots for faction
+
+    // get ship
+
+    // get faction wins
+
+    // get faction losses
+
+    // get pilot wins
+
+    // get pilot losses
+
+    // update faction wins
+
+    // update faction losses
+
+    // update pilot wins
+
+    // update pilot losses
 }
