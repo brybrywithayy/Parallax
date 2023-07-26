74ea87502af5d837c6287a222443bd8331bdfe31
brybrywithayy <brybrywithayy@gmail.com>
2023-07-25 23:51:21 -0400

    Add setters to Pilot

--- parallax/src/main/java/com/gainsay/Model/Pilot.java
+++ parallax/src/main/java/com/gainsay/Model/Pilot.java
@@ -7,6 +7,10 @@ public class Pilot {
     private int wins;
     private int losses;
 
+    public Pilot() {
+
+    }
+
     // full args constructor - Testing ONLY
     public Pilot(String name, String crime, Ship ship, int wins, int losses) {
         this.name = name;
@@ -29,19 +33,40 @@ public class Pilot {
     public String getName() {
         return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getCrime() {
         return crime;
     }
+    public void setCrime(String crime) {
+        this.crime = crime;
+    }
+
     public Ship getShip() {
         return ship;
+    }
+
+    public void setShip(Ship ship) {
+        this.ship = ship;
     }
 
     public int getWins() {
         return wins;
     }
 
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
     public int getLosses() {
         return losses;
+    }
+
+    public void setLosses(int losses) {
+        this.losses = losses;
     }
 
     // method that adds wins/losses
