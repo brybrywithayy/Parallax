1c8d9bd11d6e5baa410b051794e0f4e67233dff2
brybrywithayy <brybrywithayy@gmail.com>
2023-07-25 23:59:05 -0400

    Add fields and constructors for Controller and Service

--- parallax/src/main/java/com/gainsay/Controller/GameController.java
+++ parallax/src/main/java/com/gainsay/Controller/GameController.java
@@ -1,5 +1,24 @@
 package com.gainsay.Controller;
 
+import com.gainsay.Service.GameService;
+
+import io.javalin.Javalin;
+
 public class GameController {
     
+    GameService service;
+
+    public GameController() {
+        service = new GameService();
+    }
+
+    public Javalin startAPI() {
+        Javalin app = Javalin.create();
+        
+        // app.post
+        // app.get
+        // etc
+
+        return app;
+    }
 }
--- parallax/src/main/java/com/gainsay/Service/GameService.java
+++ parallax/src/main/java/com/gainsay/Service/GameService.java
@@ -1,5 +1,12 @@
 package com.gainsay.Service;
 
+import com.gainsay.DAO.GameDAO;
+
 public class GameService {
+    GameDAO dao;
+
+    public GameService() {
+        dao = new GameDAO();
+    }
     
 }
