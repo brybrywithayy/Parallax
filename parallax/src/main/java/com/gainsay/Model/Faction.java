9d0113aebae3f6fa0dd7e2b1e0f1bd40147fef12
brybrywithayy <brybrywithayy@gmail.com>
2023-07-25 23:45:28 -0400

    Add setters in Faction

--- parallax/src/main/java/com/gainsay/Model/Faction.java
+++ parallax/src/main/java/com/gainsay/Model/Faction.java
@@ -11,6 +11,9 @@ public class Faction {
     private int losses;
     Map<String, Pilot> pilots;
 
+    public Faction() {
+    }
+
     // full args constructor - Testing ONLY
     public Faction(String factionName, String homeSystem, String description, 
                    int wins, int losses, Map<String, Pilot> pilots) {
@@ -22,7 +25,7 @@ public class Faction {
         this.pilots = pilots;
     }
 
-    // preferred constructor
+    // preferred constructor - does not include wins and losses
     public Faction(String factionName, String homeSystem, String description,
                    Map<String, Pilot> pilots) {
         this.factionName = factionName;
@@ -37,27 +40,47 @@ public class Faction {
     public String getFactionName() {
         return factionName;
     }
+    public void setFactionName(String factionName) {
+        this.factionName = factionName;
+    }
 
     public String getHomeSystem() {
         return homeSystem;
+    }
+
+    public void setHomeSystem(String homeSystem) {
+        this.homeSystem = homeSystem;
     }
 
     public String getDescription() {
         return description;
     }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public int getWins() {
         return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
     }
 
     public int getLosses() {
         return losses;
     }
 
+    public void setLosses(int losses) {
+        this.losses = losses;
+    }
+
     public Map<String, Pilot> getPilots() {
         return pilots;
     }
 
-    // TODO: add method to retrieve pilots from database or JSON
-    // TODO: add method to add pilots into a Map and call from constructor
+    public void setPilots(Map<String, Pilot> pilots) {
+        this.pilots = pilots;
+    }
 }
